a7e532239325ddaf0e214a7b0306fd58f9cccd8f
CV104 <k22025kk@aitech.ac.jp>
2023-12-14 16:42:31 +0900

    main修正

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from diaries.DiarySample import DiarySample
-from diaries.OkuboDiary import OkuboDiaryDiary
-from diaries.IshikawaDiary import IshikawaSample
-from diaries.x22037Diary import x22037Diary
-from diaries.ShukunoDiary import ShukunoDiary
-# ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(),OkuboDiaryDiary(), ]
-
+from diaries.OkuboDiary import OkuboDiary
 from diaries.IshikawaDiary import IshikawaSample
 from diaries.x22037Diary import x22037Diary
 from diaries.ShukunoDiary import ShukunoDiary
@@ -18,7 +12,7 @@ diaries = [
     x22037Diary(),
     ShukunoDiary(),
     MoriDiary(),
-    OkuboDiaryDiary(),
+    OkuboDiary(),
 ]
 
 
