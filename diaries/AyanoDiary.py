182c22b6ca239eb8f8dfbca9b5fd60d9a335269b
Ayano <k22006kk@aitech.ac.jp>
2023-12-14 17:12:37 +0900

    Ayano-Diary

---
+++ diaries/AyanoDiary.py
@@ -0,0 +1,8 @@
+from diaries.AbstractDiary import AbstractDiary
+class AyanoDiary(AbstractDiary):
+    def get_date(self):
+        return "2023-12-14"
+    def get_summary(self):
+        return "リーダーやることになったけど、頑張ります。"
+    def get_author(self):
+        return "Ayano"
