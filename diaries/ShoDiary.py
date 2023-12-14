e28657a6c05e7b36c650730896a8983a955861d7
Ayano <k22006kk@aitech.ac.jp>
2023-12-14 17:08:37 +0900

    翔日記追加

---
+++ diaries/ShoDiary.py
@@ -0,0 +1,8 @@
+from diaries.AbstractDiary import AbstractDiary
+class ShoDiary(AbstractDiary):
+    def get_date(self):
+        return "2023-12-14"
+    def get_summary(self):
+        return "はじめまして。"
+    def get_author(self):
+        return "Sho"
