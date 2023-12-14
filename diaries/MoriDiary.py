8cc12f0f97bd92911932e1b6d60cf46165c80227
moridokin <mayumi24319@icloud.com>
2023-12-14 16:33:44 +0900

    森日記追加

---
+++ diaries/MoriDiary.py
@@ -0,0 +1,8 @@
+from diaries.AbstractDiary import AbstractDiary
+class MoriDiary(AbstractDiary):
+    def get_date(self):
+        return "2023-12-14"
+    def get_summary(self):
+        return "知り合いいないと思ってたけど、一人いてすごく嬉しかったです。"
+    def get_author(self):
+        return "Mori"
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from diaries.DiarySample import DiarySample
 from diaries.IshikawaDiary import IshikawaSample
 from diaries.x22037Diary import x22037Diary
 from diaries.ShukunoDiary import ShukunoDiary
+from diaries.MoriDiary import MoriDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
@@ -9,6 +10,7 @@ diaries = [
     IshikawaSample(),
     x22037Diary(),
     ShukunoDiary(),
+    MoriDiary(),
 ]
 
 for d in diaries:
