ce564dd9f4f6cc2d800f3b5b646e3dda42bdbfb9
Saki <k22068kk@aitech.ac.jp>
2023-12-20 08:57:10 +0900

    日記更新

--- diaries/ShukunoDiary.py
+++ diaries/ShukunoDiary.py
@@ -3,6 +3,6 @@ class ShukunoDiary(AbstractDiary):
     def get_date(self):
         return "2021-12-14"
     def get_summary(self):
-        return "朝早く起きたので眠かった"
+        return "よろしくお願いします"
     def get_author(self):
         return "Shukuno"
